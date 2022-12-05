a0e61566f098c4fa4c1cee3c3059979a9e0f4c2b
Mehdi f <m-farih@hotmail.com>
2022-12-05 15:26:43 +0100

    add balise google analytics

--- second_app/main.py
+++ second_app/main.py
@@ -3,6 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
+URL ="https://ttq8wo.deta.dev/"
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello eihfeih"
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-JY6QX9KEK7"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-251033747-1');
+    </script>
+    """
+    return prefix_google + "Hello World"
