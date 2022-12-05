90a6c5c1a5fa39e1a58f4ac73edb6b967231fafb
Mehdi f <m-farih@hotmail.com>
2022-12-05 17:07:21 +0100

    add button and tag manager

--- second_app/main.py
+++ second_app/main.py
@@ -1,20 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
 
+URL = "https://ttq8wo.deta.dev/"
 
-URL ="https://ttq8wo.deta.dev/"
-@app.route('/', methods=["GET"])
+
+@app.route('/', methods=["GET","POST"])
 def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async
-    src="https://www.googletagmanager.com/gtag/js?id=G-JY6QX9KEK7"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-251033747-1');
-    </script>
-    """
-    return prefix_google + "Hello World"
+
+
+    return render_template("index.html")
